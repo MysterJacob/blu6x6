5c8f660670abbd0f78ffac433c7f1722d0ccf9d2
MysterJacob <40712909+MysterJacob@users.noreply.github.com>
2026-07-10 16:42:03 +0200

    port naming refactor

--- mcu/common/include/port.h
+++ mcu/common/include/port.h
@@ -1,14 +1,14 @@
 #pragma once
 void port_init();
 
-typedef int (*command_handler)(int argc, char **argv);
-int port_register_command(const char *name, command_handler callback);
+typedef int (*command_handler_t)(int argc, char **argv);
+int port_register_command(const char *name, command_handler_t callback);
 int port_process_command(const char cmd[48]);
 
 typedef struct _port_cmd {
   const char *name;
-  command_handler callback;
-} port_command;
+  command_handler_t callback;
+} port_command_T;
 
 enum {
   OK = 0,
