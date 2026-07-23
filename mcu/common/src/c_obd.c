0c0786c1f8d38d4020b4f2b7b26b3a6b2cdeeec0
MysterJacob <40712909+MysterJacob@users.noreply.github.com>
2026-07-23 18:59:24 +0200

    obd pretty printing

--- mcu/common/src/c_obd.c
+++ mcu/common/src/c_obd.c
@@ -32,11 +32,11 @@ static int _list_faults_handler(int argc, char **argv)
   }
 
   if(strcmp(argv[1], "list") == 0) {
-    puts("ID              STATUS   LBP             FLAGS      QUALIFIER");
-    puts("--------------- -------- --------------- ---------- ----------");
+    puts("ID                        STATUS   LBP             FLAGS      QUALIFIER");
+    puts("------------------------- -------- --------------- ---------- ----------");
     for(size_t i = 0; i < _OBD_FAULT_COUNT; i++) {
       const obd_fault_cfg_t *obd_fault = &obd_fault_table[i];
-      printf("%-15s %-8d %-15lu %-10d 0x%-8x\n", obd_code_to_str(obd_fault->id),
+      printf("%-25s %-8d %-15lu %-10d 0x%-8x\n", obd_code_to_str(obd_fault->id),
              obd_fault->status, obd_fault->last_bootcycle_present,
              obd_fault->flags, obd_fault->qualifier);
     }
@@ -48,11 +48,11 @@ static int _list_faults_handler(int argc, char **argv)
     read_obd_faults_history(sizeof(faults_b) / sizeof(faults_b[0]), &count,
                             faults_b);
 
-    puts("ID              STATUS   LBP             FLAGS      QUALIFIER");
-    puts("--------------- -------- --------------- ---------- ----------");
+    puts("ID                        STATUS   LBP             FLAGS      QUALIFIER");
+    puts("------------------------- -------- --------------- ---------- ----------");
     for(size_t i = 0; i < count; i++) {
       const obd_fault_nv_t *fault = &faults_b[i];
-      printf("%-15s\t%-8d\t%-15lu\t%-10d :\n", obd_code_to_str(fault->id),
+      printf("%-25s\t%-8d\t%-15lu\t%-10d :\n", obd_code_to_str(fault->id),
              fault->status, fault->last_bootcycle_present, fault->qualifier);
     }
     puts("-------- -------- --------------- ---------- ----------");
